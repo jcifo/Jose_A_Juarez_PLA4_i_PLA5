0c69f328b0da24be2032af238f9636f1ba62b38e
josem <josem@COMPAQ>
2019-06-25 19:04:03 +0200

    se ha incorporado la cuenta de numeros negativos se ha dado color a la tabla en la parte superior a la diagonal

--- Pla_4_i_5/Pla3_2_7.js
+++ Pla_4_i_5/Pla3_2_7.js
@@ -7,14 +7,15 @@ function TipoNumero(){
 		  var n=Math.round(Math.random() *7)+2; 
 		  var n= n*n;
 	      for(rellCad=0;rellCad<n;rellCad++){
-	    rellenarCadena=Math.round(Math.random() *9);
-	    	 
+	    rellenarCadena=Math.random()
+	    rellenarCadena=Math.round((rellenarCadena-(0.5)) *18);
+	    // document.write(rellenarCadena+"<BR>")	 
 	         cadena=cadena + rellenarCadena; 
 
 	      }
 	document.write("Cadena"+"<BR>")
     document.write(cadena+"<BR>")
-	var nColum=Math.sqrt(cadena.length)
+	var nColum=Math.sqrt(n)
 	var valorCelda=new Array();
 
 	for(b=0;b<nColum;b++){
@@ -25,10 +26,17 @@ function TipoNumero(){
 	var posicion=-1;
 	var mayoresDeCero=0
 	var igualCero=0
+	var menoresDeCero=0
 	for(f=0;f<nColum;f++){
 		for(c=0;c<nColum;c++){
-		posicion++;	 
+		posicion++;
 		elementoCadena=cadena.charAt(posicion);	
+		if(elementoCadena=="-"){
+		posicion++;
+		elementoCadena=cadena.charAt(posicion);
+		
+		elementoCadena=("-" + elementoCadena)
+		}
 		valorCelda[f][c]=elementoCadena	;
 		if(c>f){
 			if (elementoCadena>0){
@@ -37,16 +45,20 @@ function TipoNumero(){
 			if (elementoCadena==0){
 			igualCero++
 			}
+			if (elementoCadena<0){
+				menoresDeCero++
+				}
 		}
 		}
 	    }
 		//document.write(valorCelda[f][c]+"<BR>")
 	document.write("<BR>")
+	document.write("Hay " + menoresDeCero + " numeros menores de cero por encima de la diagonal"+"<BR>"+"<BR>")
 	document.write("Hay " + mayoresDeCero + " numeros mayores de cero por encima de la diagonal"+"<BR>"+"<BR>")
 	document.write("Hay " + igualCero + " numeros iguales a cero por encima de la diagonal"+"<BR>"+"<BR>")
 	
 	///////////////////////////////////////////////////////////////////
-	var sumaDiagonal=0
+	 ///var sumaDiagonal=0;
 	 // Obtener la referencia del elemento body
 	  var body = document.getElementsByTagName("body")[0];	
 	  
@@ -59,15 +71,18 @@ function TipoNumero(){
 		  // Crea las hileras de la tabla
 	  var hilera = document.createElement("tr");
 	    for (var colum = 0; colum < nColum; colum++) {
-	    	if (fila==colum){
-	    		sumaDiagonal=sumaDiagonal+parseInt(valorCelda[fila][colum])
-	    	}
+	    	///if (fila==colum){
+	    	///	sumaDiagonal=sumaDiagonal+parseInt(valorCelda[fila][colum])
+	    	///}
 	      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
 	      // texto sea el contenido de <td>, ubica el elemento <td> al final
 	      // de la hilera de la tabla  
 	      var celda = document.createElement("td");
 	      var textoCelda = document.createTextNode(valorCelda[fila][colum]);
 	      celda.appendChild(textoCelda);
+	      if(colum>fila){
+	      celda.style.backgroundColor = "D2F2F7";
+	      }
 	      hilera.appendChild(celda);
 	      // agrega la hilera al final de la tabla (al final del elemento tblbody)
 
